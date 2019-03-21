92a254a8db7f18ff9dd1132227bece825d23c087
aluno20627 <aluno20627@ipt.pt>
2019-03-21 16:52:14 +0000

    Calculadora - continuação do controller(fez-se os botoes )

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ namespace Calculadora.Controllers
 {
     public class HomeController : Controller
     {
+        
+        double numero1, numero2,aux;
+        
         // GET: Home
         public ActionResult Index()
         {
@@ -17,6 +20,9 @@ namespace Calculadora.Controllers
 
             //variavel auxiliar
             Session["primeiraVezOperador"] = true;
+            Session["num1"] = 0.0;
+            Session["limpaVisor"] = false;
+            Session["op"] = "";
             return View();
         }
         
@@ -40,6 +46,15 @@ namespace Calculadora.Controllers
                 case "7":
                 case "8":
                 case "9":
+
+                    //apaga ecrã depois do operador ser pressionado a 1ª vez
+                    if ((bool)Session["limpaVisor"] == true)
+                    {
+
+                        ecra = "";
+                        Session["limpaVisor"] = false;
+
+                    }
                     //vou decidir o que fazer quando no visor só existe o 'zero'
 
                     if (visor == "0")  //outra opção seria visor.Equals("0")
@@ -50,6 +65,7 @@ namespace Calculadora.Controllers
                     {//visor e bt são strings por isso esta operação concatena as duas
                         ecra = ecra + bt;
                     }
+
                     break;
                 //processar o caso da ','
                 case ",":
@@ -64,10 +80,89 @@ namespace Calculadora.Controllers
                     //é a 1ª vez que carreguei num destes operadores?
                     if ((bool)Session["primeiraVezOperador"] == true)
                     {
+                        Session["primeiraVezOperador"] = false;
+                        Session["num1"] = ecra;
+                        Session["op"] = bt;
+                        Session["limpaVisor"] = true;
+
+                    }
+                    else
+                    {
+                        switch (Session["op"])
+                        {
+                            case "+":
+
+                                Session["num1"] = Convert.ToDouble(Session["num1"]) + Convert.ToDouble(ecra);
+                                break;
+                            case "-":
+                               
+                                Session["num1"] = Convert.ToDouble(Session["num1"]) + Convert.ToDouble(ecra);
+                                break;
+                            case "x":
+                               
+                                Session["num1"] = Convert.ToDouble(Session["num1"]) + Convert.ToDouble(ecra);
+                                break;
+                            case ":":
+                                
+                                Session["num1"] = Convert.ToDouble(Session["num1"]) + Convert.ToDouble(ecra);
+                                break;
+
+                        }
+                        
+                        Session["op"] = bt;
+                        Session["limpaVisor"] = true;
 
                     }
 
+
                     break;
+
+
+                case "=":
+
+
+                    numero2 = Convert.ToDouble(ecra);
+                    numero1 = (double)Session["num1"];
+
+                    switch (Session["op"])
+                    {
+                        case "+":
+                            aux = numero1 + numero2;
+                            ecra = "" + aux;
+                            break;
+                        case "-":
+                            aux = numero1 - numero2;
+                            ecra = "" + aux;
+                            break;
+                        case "x":
+                            aux = numero1 * numero2;
+                            ecra = "" + aux;
+                            break;
+                        case ":":
+                            aux = numero1 / numero2;
+                            ecra = "" + aux;
+                            break;
+
+                    }
+                    Session["primeiraVezOperador"] = true;
+                    Session["limpaVisor"] = true;
+
+
+                    
+                    break;
+
+                case "+/-":
+                    ecra = "" + Convert.ToDouble(ecra) * -1;
+                    break;
+
+                case "C":
+                    ecra = "0";
+                    numero1 = 0;
+                    numero2 = 0;
+                    Session["op"] = "";
+                    Session["num1"] = 0;
+                    break;
+                    
 
 
             }
